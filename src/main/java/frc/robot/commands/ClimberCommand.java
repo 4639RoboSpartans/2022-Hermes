f91ac3db90cc270609b0e500866b8b3bd0b8481a
JonnyJonnyBoy <n.jonathan.jiang@gmail.com>
2022-02-05 16:22:30 -0600

    .

--- src/main/java/frc/robot/commands/ClimberCommand.java
+++ src/main/java/frc/robot/commands/ClimberCommand.java
@@ -1,7 +1,33 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ClimberSubsystem;
 
 public class ClimberCommand extends CommandBase{
+    private ClimberSubsystem m_climber = new ClimberSubsystem();
+
+    public ClimberCommand(){
+        addRequirements(m_climber);
+    }
+
+    @Override
+    public void initialize(){
+        m_climber.setClimber(0, 0);
+    }
+
+    @Override
+    public void execute(){
+        m_climber.setClimber(0.2, 0.2);
+    }
     
+    @Override
+    public void end(boolean Interrupted){
+        m_climber.stopClimber();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+
 }
