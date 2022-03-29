478cca8da358cdce0561e2fe89d8b19fa8e1e8c1
JonnyJonnyBoy <60590076+JonnyJonnyBoy@users.noreply.github.com>
2022-03-28 21:17:14 -0500

    cahnges

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -3,12 +3,17 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ClimberSubsystem extends SubsystemBase {
     private WPI_VictorSPX leftClimberMotor = new WPI_VictorSPX(Constants.LClimberMotor);
     private WPI_VictorSPX rightClimberMotor = new WPI_VictorSPX(Constants.RClimberMotor);
+
+    public DoubleSolenoid leftClimberSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,2,3);
+    public DoubleSolenoid rightClimberSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,4,5);
 
     public ClimberSubsystem(){
         leftClimberMotor.configFactoryDefault();
@@ -27,6 +32,15 @@ public class ClimberSubsystem extends SubsystemBase {
     }
     public void setRightClimber(double rightSpeed){
         rightClimberMotor.set(rightSpeed);
+    }
+
+    public void extendPistons(){
+        leftClimberSolenoid.set(DoubleSolenoid.Value.kForward);
+        rightClimberSolenoid.set(DoubleSolenoid.Value.kForward);
+    }
+    public void retractPistons(){
+        leftClimberSolenoid.set(DoubleSolenoid.Value.kReverse);
+        rightClimberSolenoid.set(DoubleSolenoid.Value.kReverse);
     }
 
     public void stopClimber(){
